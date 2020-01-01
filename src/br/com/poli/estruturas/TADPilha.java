1607d8b498681161f9c4a9d5fde15caa3c6270b3
AltheusV <matheus.albert@hotmail.com>
2020-01-01 17:18:13 -0300

    exercicio completo

---
+++ src/br/com/poli/estruturas/TADPilha.java
@@ -0,0 +1,12 @@
+package br.com.poli.estruturas;
+
+public interface TADPilha <T>{
+	
+	//public void print();
+	public void push(T info);
+	public T pop();
+	public T top();
+	public int size();
+	public boolean isEmpty();
+	
+}
